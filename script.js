3bcf9e851ba401e7cc09b60310f151655d19fc93
adem <adem0946chaa@gmail.com>
2022-08-16 08:40:52 +0100

    responsive

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ window.onscroll = () => {
     el.style.backgroundColor = "transparent";
   }
 };
+const menuchange = function (e) {
+  console.log(e.checked);
+  if (e.checked) {
+    document.getElementsByClassName("navbar")[0].style.left = "0";
+  } else {
+    document.getElementsByClassName("navbar")[0].style.left =
+      "calc(-60vw - 40px)";
+  }
+};
+document.getElementById("checkbox").addEventListener("change", (e) => {
+  menuchange(e.target);
+});
+document.getElementsByClassName("close")[0].addEventListener("click", () => {
+  const e = document.getElementById("checkbox");
+  e.checked = false;
+  menuchange(e);
+});
