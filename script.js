315c3e53417933b34399558d4c5fc0247f0c1c2b
adem <adem0946chaa@gmail.com>
2022-08-19 12:16:07 +0100

    fixed mobile bug

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
-if (
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  )
-) {
-  window.onscroll = () => {
-    const el = document.getElementsByClassName("header")[0];
-    if (
-      document.body.scrollTop > 40 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      el.style.backgroundColor = "black";
-    } else {
-      el.style.backgroundColor = "transparent";
-    }
-  };
-} else {
-  window.onscroll = () => {
-    const el = document.getElementsByClassName("header")[0];
-    if (
-      document.body.scrollTop > 40 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      el.style.padding = "0px 40px";
-      el.style.backgroundColor = "black";
-    } else {
-      el.style.padding = "30px 40px";
-      el.style.backgroundColor = "transparent";
-    }
-  };
-}
+window.onscroll = () => {
+  const el = document.getElementsByClassName("header")[0];
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 20) {
+    el.style.padding = "0px 40px";
+    el.style.backgroundColor = "black";
+  } else {
+    el.style.padding = "30px 40px";
+    el.style.backgroundColor = "transparent";
+  }
+};
 
 const menuchange = function (e) {
   console.log(e.checked);
